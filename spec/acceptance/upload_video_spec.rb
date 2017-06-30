2e60ba228e0768c66efdb6d0ba2220ad3bcd87e0
Philipe <philipesousacortez@gmail.com>
2017-06-30 18:24:19 -0300

    first unit spec

--- spec/acceptance/upload_video_spec.rb
+++ spec/acceptance/upload_video_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe 'video upload', type: :feature do
 
   context 'when the user upload a valid video' do
 
-    it 'record a video and returns the recorded video id' do
-      pending "need to implement Video class"
+    it 'record a video and return to the videos page' do
       visit videos_path
       click_on 'New video'
       expect(page).to have_current_path(new_video_path)
@@ -13,13 +12,11 @@ RSpec.describe 'video upload', type: :feature do
       within("#new_video") do
         fill_in "Title", with: "Test video"
         fill_in "Description", with: "a simple video description"
-        attach_file('Video', File.join(Rails.root, 'spec', 'fixtures', 'acceptance', 'test_video.mp4'))
+        attach_file('Url', File.join(Rails.root, 'spec', 'fixtures', 'acceptance', 'test_video.mp4'))
         click_on 'Create'
       end
-    end
 
-    it 'returns status 201 created' do
+      expect(page).to have_current_path(videos_path)
     end
   end
-  
 end
